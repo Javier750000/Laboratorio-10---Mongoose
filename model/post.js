1e06d31475f8921cba3258fe44e18c3f72aa1e3b
Javier Sánchez Vázquez de Mercado <43384997+JavierSanchezVazquezdeMercado@users.noreply.github.com>
2021-05-20 22:35:56 -0500

    Add files via upload

---
+++ model/post.js
@@ -0,0 +1,9 @@
+const {Schema, model} = require("mongoose");
+const postSchema = Schema({
+    title: String,
+    author: String,
+    post_date: {type: Date, default: Date.now},
+    post_data: String
+});
+
+module.exports = model('posts', postSchema);
